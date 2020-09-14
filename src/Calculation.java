00604476bfa869075a1b3abc97082098ed3dc347
Vanessa <va.au@gmx.at>
2020-09-14 11:04:34 +0200

    second try, manual logic

--- src/Calculation.java
+++ src/Calculation.java
@@ -7,21 +7,47 @@ public class Calculation {
         Scanner scanner = new Scanner(System.in);
 
         int[] allNumbers = {};
-        int number = 0, pos = 0;
+        int[] temp = {0};
+        int number = 0;
+        int pos = 0;
+        int count = 1;
+        int idx = 0;
 
-
-
-        while (!scanner.hasNext("q")){
+        /*while (!scanner.hasNext("q")) {
             number = scanner.nextInt();
 
             allNumbers = Arrays.copyOf(allNumbers, allNumbers.length + 1);
             allNumbers[pos] = number;
 
             pos++;
+        }*/
+
+        while (!scanner.hasNext("q")) {
+            number = scanner.nextInt();
+            allNumbers = new int[count];
+
+            for (int i = 0; i < allNumbers.length - 1; i++) {
+                allNumbers[i] = temp[i];
+            }
+            allNumbers[pos] = number;
+            temp = allNumbers;
+
+
+            count++;
+            pos++;
         }
+
         System.out.println(Arrays.toString(allNumbers));
-        Arrays.sort(allNumbers);
-        System.out.println("Highest Number:" + allNumbers[allNumbers.length -1]);
+
+        int highest = 0;
+
+        for (int curNumber : allNumbers) {
+            if (curNumber > highest) {
+                highest = curNumber;
+            }
+        }
+
+        System.out.println("Highest Number:" + highest);
 
     }
 }
